050f8bbb6bc73ce0bfe8c6c25e1ea1d45c4d3b5d
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2023-02-02 02:12:16 +0530

    initialized DB

---
+++ backend/init.sql
@@ -0,0 +1,54 @@
+CREATE TABLE location
+(
+    loc_id      BIGSERIAL   NOT NULL,
+    latitude    FLOAT       NOT NULL,
+    longitude   FLOAT       NOT NULL,
+    PRIMARY KEY (loc_id),
+    UNIQUE      (latitude, longitude)
+);
+
+CREATE TABLE rider
+(
+    rider_id    BIGSERIAL   NOT NULL,
+    name        VARCHAR(50) NOT NULL,
+    contact     DECIMAL     NOT NULL,
+    PRIMARY KEY (rider_id),
+    UNIQUE      (contact)
+);
+
+CREATE TABLE bag
+(
+    bag_id      BIGSERIAL   NOT NULL,
+    type        BOOL        NOT NULL,
+    PRIMARY KEY (bag_id)
+);
+
+CREATE TABLE package
+(
+    object_id       BIGSERIAL   NOT NULL,
+    weight          FLOAT       NOT NULL,
+    length          FLOAT,
+    breadth         FLOAT,
+    height          FLOAT,
+    delivery_date   TIMESTAMP    NOT NULL,
+    delivery_loc    BIGINT      NOT NULL,
+    erroneous       BOOL        NOT NULL    DEFAULT true,
+    comments        VARCHAR(200),
+    type            BOOL        NOT NULL,
+    completed       BOOL        NOT NULL    DEFAULT false,
+    PRIMARY KEY (object_id),
+    FOREIGN KEY (delivery_loc)  REFERENCES location(loc_id)
+);
+
+CREATE TABLE tour
+(
+    tour_id             BIGSERIAL   NOT NULL,
+    assigned_rider      BIGINT      NOT NULL,
+    assigned_bag        BIGINT      NOT NULL,
+    delivery            BIGINT      NOT NULL,
+    delivery_order      INT         NOT NULL,
+    PRIMARY KEY (tour_id),
+    FOREIGN KEY (assigned_rider)    REFERENCES rider(rider_id), 
+    FOREIGN KEY (assigned_bag)      REFERENCES bag(bag_id),
+    FOREIGN KEY (delivery)          REFERENCES package(object_id)
+);
