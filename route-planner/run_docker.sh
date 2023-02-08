951437a9e17882be3b2c2473ce9bf50d9573f9e5
Sumanth NR <sumanthnr62@gmail.com>
2023-02-08 12:45:55 +0530

    Migrated to Karnataka .osm.pbf file

--- route-planner/run_docker.sh
+++ route-planner/run_docker.sh
@@ -1,7 +1,7 @@
-sudo docker run -t -v "${PWD}/data:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/southern-zone-latest.osm.pbf
+docker run -t -v "${PWD}/data_karnataka:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/karnataka-latest.osm.pbf
 
-sudo docker run -t -v "${PWD}/data:/data" osrm/osrm-backend osrm-partition /data/southern-zone-latest.osrm
+docker run -t -v "${PWD}/data_karnataka:/data" osrm/osrm-backend osrm-partition /data/karnataka-latest.osrm
 
-sudo docker run -t -v "${PWD}/data:/data" osrm/osrm-backend osrm-customize /data/southern-zone-latest.osrm
+docker run -t -v "${PWD}/data_karnataka:/data" osrm/osrm-backend osrm-customize /data/karnataka-latest.osrm
 
-sudo docker run -t -i -p 5000:5000 -v "${PWD}/data:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/southern-zone-latest.osrm --max-viaroute-size 15000 --max-trip-size 3000 --max-table-size 3000 --max-matching-size 3000 --max-nearest-size 3000 --max-alternatives 10 --max-matching-radius -1
+docker run -t -i -p 5000:5000 -v "${PWD}/data:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/karnataka-latest.osrm --max-viaroute-size 15000 --max-trip-size 3000 --max-table-size 3000 --max-matching-size 3000 --max-nearest-size 3000 --max-alternatives 10 --max-matching-radius -1
